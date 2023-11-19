f0aff95274dd2a06ee749d8486aea23637b88506
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-11-19 11:39:41 +0100

    first changes

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 print ('Hello Wolrd ')
 import streamlit as st
+# Title of the app
+st.title('My Streamlit App')
+
+# Adding text to the app
 st.write('Welcome to my first Streamlit app!')
+
+# Adding a slider widget
+slider_value = st.slider('Select a value', 0, 100, 50)
+
+# Displaying the value selected on the slider
+st.write('You selected:', slider_value)
