318b58fa64b61af3dcc39b85e2854529653c9203
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-11-22 15:40:33 +0100

    new

--- app.py
+++ app.py
@@ -1,42 +1,27 @@
 
 import streamlit as st
 import calendar
-import datetime
 
-# Title of the app
-st.title('Calendar View')
+def display_monthly_calendar(year, month):
+    cal = calendar.monthcalendar(year, month)
+    month_name = calendar.month_name[month]
 
-# Get today's date
-today = datetime.date.today()
+    st.title(f"Kalender für {month_name} {year}")
 
-# Get the first day of the current month
-first_day_of_month = today.replace(day=1)
+    col_width = 50
 
-# Get the last day of the current month
-last_day_of_month = first_day_of_month.replace(
-    month=first_day_of_month.month % 12 + 1,
-    day=1) - datetime.timedelta(days=1)
+    # Tabellenkopf mit den Wochentagen
+    st.write("|".join(["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]))
+    st.write("-" * (col_width * 7))
 
-# Create a header with the name of the month and year
-st.header(first_day_of_month.strftime('%B %Y'))
+    # Darstellung des Kalenders
+    for week in cal:
+        week_str = "|".join([str(day) if day != 0 else "" for day in week])
+        st.write(week_str)
 
-# Create a layout to display the days of the current month
-days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+# Streamlit App
+st.title("Monatskalender")
+year = st.number_input("Jahr eingeben", min_value=1900, max_value=2100, value=2023)
+month = st.slider("Monat auswählen", 1, 12, 1)
 
-col_width = st.columns(7)
-for i, day in enumerate(days_of_week):
-    col_width[i].write(day)
-
-# Create a list to hold the days of the month
-days_list = []
-
-# Fill in the matrix with the days of the month
-for day in range(1, last_day_of_month.day + 1):
-    date = first_day_of_month + datetime.timedelta(days=day - 1)
-    week_day = date.weekday()  # Get the day of the week (0 - Monday, 6 - Sunday)
-    days_list.append((date, week_day))
-
-# Display the days of the month in the layout
-for date, week_day in days_list:
-    cell = st.columns(7)[week_day]
-    cell.write(f"{date.day}")
+display_monthly_calendar(year, month)
