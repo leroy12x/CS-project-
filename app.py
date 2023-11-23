de32f75c2759ba5d14aa49b26f1a9851718a91bf
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-11-23 19:46:35 +0100

    new 1

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
 import calendar
 
 # Funktion zur Anzeige des Kalenders für den ausgewählten Monat
-def display_monthly_calendar(year, month):
+def display_monthly_calendar(year, month, tasks):
     cal = calendar.monthcalendar(year, month)
     month_name = calendar.month_name[month]
 
@@ -23,7 +22,9 @@ def display_monthly_calendar(year, month):
         table += "<tr>"
         for day in week:
             if day != 0:
-                table += f"<td style='border: 1px solid black; padding: 8px; text-align: center;'>{day}</td>"
+                tasks_for_day = tasks.get((year, month, day), [])
+                task_info = "<br>".join([f"{task['time']}, {task['duration']}" for task in tasks_for_day])
+                table += f"<td style='border: 1px solid black; padding: 8px; text-align: left; vertical-align: top;'>{day}<br>{task_info}</td>"
             else:
                 table += "<td style='border: 1px solid black; padding: 8px;'></td>"
         table += "</tr>"
@@ -35,11 +36,26 @@ def display_monthly_calendar(year, month):
 def display_task_manager():
     st.title("Taskmanager")
     task_date = st.date_input("Datum auswählen", key="task_date")
+    task_time = st.time_input("Uhrzeit eingeben", key="task_time")
+    task_duration = st.text_input("Dauer eingeben (z.B. 1 Stunde)", key="task_duration")
     task_description = st.text_input("Aufgabenbeschreibung eingeben", key="task_description")
 
     if st.button("Aufgabe hinzufügen"):
-        # Hier könntest du den Code einfügen, um die Aufgabe zu speichern oder zu verarbeiten
-        st.success(f"Aufgabe für {task_date}: '{task_description}' hinzugefügt!")
+        task_info = {
+            'time': str(task_time),
+            'duration': task_duration,
+            'description': task_description
+        }
+        tasks = st.session_state.get('tasks', {})
+        date_key = (task_date.year, task_date.month, task_date.day)
+        if date_key in tasks:
+            tasks[date_key].append(task_info)
+        else:
+            tasks[date_key] = [task_info]
+        st.session_state.tasks = tasks
+        st.success(f"Aufgabe für {task_date} um {task_time} hinzugefügt!")
+
+        st.experimental_rerun()
 
 # Navigation zwischen den Seiten
 def main():
@@ -58,7 +74,8 @@ def main():
         ]
         selected_month = st.selectbox("Monat auswählen", month_names, key="selected_month")
         month_index = month_names.index(selected_month) + 1
-        display_monthly_calendar(year, month_index)
+        tasks = st.session_state.get('tasks', {})
+        display_monthly_calendar(year, month_index, tasks)
     elif app_mode == "Taskmanager":
         display_task_manager()
 
