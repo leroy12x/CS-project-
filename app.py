d8888214ec434a0f84814d846ef0ad8741a3267f
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-11-23 20:25:55 +0100

    new clander

--- app.py
+++ app.py
@@ -9,32 +9,19 @@ def display_monthly_calendar(year, month, tasks):
 
     st.title(f"Kalender für {month_name} {year}")
 
-    # Erstelle eine leere Tabelle für den Kalender
-    table = "<table style='width:100%; border-collapse: collapse;'>"
-
-    # Tabellenkopf mit den Wochentagen
-    table += "<tr>"
-    for day in ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]:
-        table += f"<th style='border: 1px solid black; padding: 8px; text-align: center;'>{day}</th>"
-    table += "</tr>"
-
     # Darstellung des Kalenders
     for week in cal:
-        table += "<tr>"
         for day in week:
             if day != 0:
                 tasks_for_day = tasks.get((year, month, day), [])
-                task_info = "<br>".join([f"{task['time']} - {task['end_time']}: {task['description']}" for task in tasks_for_day])
-                table += f"<td style='border: 1px solid black; padding: 8px; text-align: left; vertical-align: top; height: 100px;'>"
-                table += f"<a href='javascript:void(0)' onclick=\"document.getElementById('details-{year}-{month}-{day}').style.display='block'\" style='text-decoration: none; color: black;'>{day}</a>"
-                table += f"<div style='display: none; position: absolute; background-color: white; border: 1px solid black; padding: 8px;' id='details-{year}-{month}-{day}'>{task_info}</div>"
-                table += "</td>"
-            else:
-                table += "<td style='border: 1px solid black; padding: 8px;'></td>"
-        table += "</tr>"
+                task_info = "\n".join([f"{task['time']} - {task['end_time']}: {task['description']}" for task in tasks_for_day])
 
-    table += "</table>"
-    st.write(table, unsafe_allow_html=True)
+                # Anzeige des Tages und der Aufgaben in einer Spalte
+                col1, col2 = st.beta_columns(2)
+                col1.subheader(f"{day}. {month_name}")
+                with col2:
+                    st.write(task_info)
+                    st.write("---")
 
 # Funktion zur Anzeige der Aufgabenübersicht und zum Löschen von Aufgaben
 def display_task_overview():
