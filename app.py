95c53624c2536787d717589c591157b448a3f0e6
markklb <152989960+markklb@users.noreply.github.com>
2023-12-05 15:37:04 +0100

    new

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ def display_weekly_calendar(year, month, week, tasks):
     # Commented out for the To Do List version
     pass
 
-# Function to display the task overview and delete tasks
 def display_task_overview():
-    # Set the title for the section in the Streamlit app
     st.title("To Do List")
-
-    # Retrieve the tasks from the CSV file
     tasks = load_tasks_from_csv()
 
-    # Sort tasks based on grade relevance (descending order)
-    sorted_tasks = sorted((task for day_tasks in tasks.values() for task in day_tasks), key=lambda x: x['ects'] * (x['percentage'] / 100), reverse=True)
-    
-    # Display sorted tasks
-    for idx, task in enumerate(sorted_tasks):
-        st.write(f"{idx + 1}. Description: {task['description']}, ECTS: {task['ects']}, Percentage: {task['percentage']}%, Grade Relevance: {task['ects'] * (task['percentage'] / 100)}, Due Date: {task['due_date']}")
-
+    for day, day_tasks in tasks.items():
+        st.subheader(f"Tasks for {day}")
+        for task in day_tasks:
+            if task['completed']:
+                st.write(f"Completed: {task['description']}")
+            else:
+                overdue = datetime.strptime(task['due_date'], '%Y-%m-%d') < datetime.now()
+                st.write(f"{task['description']} - Due: {task['due_date']} {'(Overdue)' if overdue else ''}")
+                if st.button(f"Mark as Completed", key=f"complete_{task['description']}"):
+                    task['completed'] = True
+                    save_tasks_to_csv(tasks)
+                    st.experimental_rerun()
 # Function to display the task manager (now renamed to Create Tasks)
 def display_task_manager():
     st.title("Create Tasks")  # Renamed from "Task Manager"
@@ -50,6 +51,7 @@ def display_task_manager():
             'ects': task_ects,
             'percentage': task_percentage,
             'due_date': task_due_date  # Use 'due_date' instead of 'end_time' for the due date
+            'completed': False
         }
 
         date_key = (start_date_time.year, start_date_time.month, start_date_time.day)
@@ -92,42 +94,34 @@ def compute_start_time(tasks, due_date):
 
 # Function to save tasks to a CSV file
 def save_tasks_to_csv(tasks):
-    # Convert tasks to a dataframe
-    df = pd.DataFrame([(key[0], key[1], key[2], task['time'], task['end_time'], task['duration'], task['description'], task['ects'], task['percentage'], task['due_date'])
+    df = pd.DataFrame([(key[0], key[1], key[2], task['time'], task['end_time'], task['duration'], task['description'], task['ects'], task['percentage'], task['due_date'], task['completed'])
                        for key, tasks_list in tasks.items() for task in tasks_list],
-                      columns=['Year', 'Month', 'Day', 'Time', 'End Time', 'Duration', 'Description', 'ECTS', 'Percentage', 'Due Date'])
-
-    # Save the dataframe to a CSV file
+                      columns=['Year', 'Month', 'Day', 'Time', 'End Time', 'Duration', 'Description', 'ECTS', 'Percentage', 'Due Date', 'Completed'])
     df.to_csv('tasks.csv', index=False)
 
 # Function to load tasks from a CSV file
 def load_tasks_from_csv():
     try:
-        # Load the CSV file into a dataframe
         df = pd.read_csv('tasks.csv')
-
-        # Convert the dataframe to the tasks dictionary
         tasks = {}
         for _, row in df.iterrows():
-            date_key = (int(row.get('Year', 0)), int(row.get('Month', 0)), int(row.get('Day', 0)))
+            date_key = (int(row['Year']), int(row['Month']), int(row['Day']))
             task_info = {
-                'time': row.get('Time', ''),
-                'end_time': row.get('End Time', ''),
-                'duration': row.get('Duration', ''),
-                'description': row.get('Description', ''),
-                'ects': row.get('ECTS', 0),
-                'percentage': row.get('Percentage', 0),
-                'due_date': row.get('Due Date', '')
+                'time': row['Time'],
+                'end_time': row['End Time'],
+                'duration': row['Duration'],
+                'description': row['Description'],
+                'ects': row['ECTS'],
+                'percentage': row['Percentage'],
+                'due_date': row['Due Date'],
+                'completed': row.get('Completed', False)  # Ensure to handle the 'completed' field
             }
             if date_key in tasks:
                 tasks[date_key].append(task_info)
             else:
                 tasks[date_key] = [task_info]
-
         return tasks
-
     except FileNotFoundError:
-        # If the file doesn't exist, return an empty dictionary
         return {}
         
 
