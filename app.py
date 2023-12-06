f0d1ac966c4323328fa53a45620929569d1f1036
markklb <152989960+markklb@users.noreply.github.com>
2023-12-06 23:02:13 +0100

    n

--- app.py
+++ app.py
@@ -230,41 +230,32 @@ from datetime import datetime, timedelta
 
 def display_weekly_calendar():
     st.title("Weekly Calendar")
-    tasks = load_tasks_from_csv()
+    tasks = load_tasks_from_csv()  # Ensure this function returns a dictionary of tasks
 
     today = datetime.today()
     start_of_week = today - timedelta(days=today.weekday())
     days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-    # Create a dictionary to hold tasks for each day
-    week_tasks = {day: [] for day in days}
-    for i in range(7):
-        current_day = start_of_week + timedelta(days=i)
-        if (current_day.year, current_day.month, current_day.day) in tasks:
-            week_tasks[days[current_day.weekday()]].extend(tasks[(current_day.year, current_day.month, current_day.day)])
-
-    # Display the calendar
     cols = st.columns(7)
     for i, day in enumerate(days):
         with cols[i]:
             st.subheader(day)
-            # Display the date
             day_date = start_of_week + timedelta(days=i)
             st.write(day_date.strftime('%b %d'))
 
-            # Display tasks for that day
-            day_tasks = week_tasks[day]
-            if day_tasks:
-                for task in day_tasks:
-                    due_date = datetime.strptime(task['due_date'], '%Y-%m-%d')
-                    overdue = due_date < today
-                    # Styling based on task completion
-                    if overdue:
-                        st.markdown(f"<span style='color: red;'>{task['description']}</span>", unsafe_allow_html=True)
-                    elif completed:
-                        st.markdown(f"<span style='color: green;'>{task['description']}</span>", unsafe_allow_html=True)
-                    else:
-                        st.markdown(f"<span style='color: yellow;'>{task['description']}</span>", unsafe_allow_html=True)
+            day_tasks = tasks.get((day_date.year, day_date.month, day_date.day), [])
+            for task in day_tasks:
+                due_date = datetime.strptime(task['due_date'], '%Y-%m-%d')
+                overdue = due_date < today
+                completed = task.get('completed', False)  # Access the completed status
+                
+                # Apply color styling based on the task status
+                if completed:
+                    st.markdown(f"<span style='color: green;'>{task['description']}</span>", unsafe_allow_html=True)
+                elif overdue:
+                    st.markdown(f"<span style='color: red;'>{task['description']}</span>", unsafe_allow_html=True)
+                else:
+                    st.markdown(f"<span style='color: yellow;'>{task['description']}</span>", unsafe_allow_html=True)
                     
                     
             else:
