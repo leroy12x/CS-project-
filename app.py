221d4984365a2ab3f9c30c16211213f3fdb13385
markklb <152989960+markklb@users.noreply.github.com>
2023-12-07 16:23:34 +0100

    nigg

--- app.py
+++ app.py
@@ -293,15 +293,13 @@ def get_current_semester():
         print("Error calling API: ", response.status_code)
         return None
 
-# Streamlit app setup
-st.title('Current Semester Information')
 
 # Fetch and display the current semester
 semester_info = get_current_semester()
 if semester_info:
     # Extract and display the description from the semester information
     semester_description = semester_info.get('description', 'No description available')
-    st.write(f"Semester Description: {semester_description}")
+    st.write(f" {semester_description}")
 else:
     st.error("Failed to fetch current semester information.")
 
