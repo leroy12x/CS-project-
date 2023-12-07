966ac2b73ac23234bf7d66ffa2a05aba1c938a38
markklb <152989960+markklb@users.noreply.github.com>
2023-12-07 17:11:04 +0100

    n

--- app.py
+++ app.py
@@ -303,34 +303,6 @@ def get_current_semester():
         else:
             st.error("Failed to fetch current semester information.")
 
-def get_current_semester():
-    url = "https://integration.preprod.unisg.ch/eventapi/timeLines/currentTerm"
-    headers = {
-        "X-ApplicationId": "587acf1c-24d0-4801-afda-c98f081c4678",
-        "API-Version": "1",
-        "X-RequestedLanguage": "de"
-    }
-    response = requests.get(url, headers=headers)
-
-    if response.ok:
-        # If the response was successful, no Exception will be raised
-        json_data = response.json()
-        return json_data
-    else:
-        # If the response was not successful, print the error code
-        print("Error calling API: ", response.status_code)
-        return None
-
-# Use the function in your Streamlit code
-current_semester = get_current_semester()
-if current_semester:
-    # Do something with the data, like displaying it in Streamlit
-    st.write(current_semester)
-else:
-    # Handle the error case
-    st.error("Failed to retrieve data.")
-
-
 
 def main():
     st.sidebar.title("Navigation")
