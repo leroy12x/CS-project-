978d127ca5bd0441141f59565dfea5b50b9c2f24
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-12-08 13:55:34 +0100

    new

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def display_task_manager():
         task_ects = st.number_input("Enter ECTS Points", min_value=0, key="task_ects")
     else: 
         task_ects = course_ects
-        st.write(f'Enter Task Description\n{task_ects}')
+        st.markdown(f'Enter ECTS Points<br>{task_ects}', unsafe_allow_html=True)
            
     task_percentage = st.number_input("Enter Percentage of Grade", min_value=0, max_value=100, key="task_percentage")
             
