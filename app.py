30051970d24972661e69efeaaf2ebce9c2b28e7f
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-12-10 18:02:32 +0100

    gs

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def display_task_overview():
     tasks = st.session_state.tasks
     calculate_ects_percentage(tasks)
     for day, day_tasks in tasks.items():
-        st.subheader(f"Tasks for {day.strftime('%d.%m.%Y')}") 
+        st.subheader(f"Tasks for {day}") 
         for task in day_tasks:
             task_name = task['name']
             task_key = f"complete_{task_name}_{day}"
