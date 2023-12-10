59f135a504cf4aff38e6101ec111851557453ef2
sral123 <152025035+sral123@users.noreply.github.com>
2023-12-10 16:45:30 +0100

    1

--- app.py
+++ app.py
@@ -275,12 +275,12 @@ def display_task_overview():
 
     # Function to handle marking tasks as completed
     def mark_as_completed(task_name, due_date):
-    for day, day_tasks in st.session_state.tasks.items():
-        for task in day_tasks:
-            if task['name'] == task_name and task['due_date'] == due_date:
-                task['completed'] = True
-                save_tasks_to_csv(st.session_state.tasks)  # Save the updated tasks
-                break
+        for day, day_tasks in st.session_state.tasks.items():
+            for task in day_tasks:
+                if task['name'] == task_name and task['due_date'] == due_date:
+                    task['completed'] = True
+                    save_tasks_to_csv(st.session_state.tasks)  # Save the updated tasks
+                    break
 
 
 
