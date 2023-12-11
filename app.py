080f42ac56dff5232bb7d9ed03d2e6490d6736a7
markklb <152989960+markklb@users.noreply.github.com>
2023-12-11 20:04:23 +0100

    n

--- app.py
+++ app.py
@@ -54,10 +54,12 @@ def get_events_by_term(term_id):
     }
     response = requests.get(url2, headers=headers)
     if response.ok:
-        return pd.DataFrame(response.json())
-    else:
-        st.error(f"Error calling API: {response.status_code}")
-        return pd.DataFrame()
+    print(response.text)  # Add this line to debug
+    return pd.DataFrame(response.json())
+else:
+    print(response.text)  # Add this line to debug
+    st.error(f"Error calling API: {response.status_code}")
+    return pd.DataFrame()
 
 
   # Load tasks from CSV if they exist, else initialize as empty dictionary
@@ -126,7 +128,7 @@ def display_task_manager():
             task_ects = int(task_ects)
         if task_id is not None:
             term_id = semester_id
-            events_df = pd.DataFrame()
+            events_df = get_events_by_term(term_id)
             # Filter events by the provided course ID
             if not events_df.empty:
                 # Ensure the course_id is a string and remove any leading/trailing whitespace
