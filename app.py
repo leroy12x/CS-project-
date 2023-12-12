6d54ba7f21aaa6606f4bec6e0819b60b961d1950
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-12-12 15:48:47 +0100

    ok

--- app.py
+++ app.py
@@ -139,9 +139,10 @@ def display_task_manager():
                     title_list = course_events['title'].tolist()
                     if title_list and isinstance(title_list[0], str):
                         task_name = title_list[0]  # Set the title as task description
-                        max_credits_list = course_events['maxCredits'].tolist()
-                        if max_credits_list and isinstance(max_credits_list[0], list) and len(max_credits_list[0]) > 0:
-                            task_ects = (int(max_credits_list[0][0])/100) # Set maxCredits as ECTS
+                   max_credits_list = course_events['maxCredits'].tolist()
+                    if max_credits_list and isinstance(max_credits_list[0], list) and len(max_credits_list[0]) > 0:
+                        max_credits = int(max_credits_list[0][0])
+                        st.write(max_credits) # Set maxCredits as ECTS
                     else:
                         st.error(f"No maxCredits found for Course ID: {course_id}")
                 else:
