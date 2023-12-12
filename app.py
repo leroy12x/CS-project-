4c2b5da7f8e5206478a780daed4a2ed3ec2f9034
markklb <152989960+markklb@users.noreply.github.com>
2023-12-12 16:51:29 +0100

    n

--- app.py
+++ app.py
@@ -270,19 +270,10 @@ def edit_tasks():
                 del tasks[selected_date_key]
             save_tasks_to_csv(tasks)
             st.success("Task deleted successfully!")
-        
-        
-        
-        
-            
-
-
-                    
 
 
 
  
-      # Function to handle marking tasks as completed
 def mark_as_completed(task_description, day):
         for task in st.session_state.tasks[day]:
             if task['description'] == task_description:
