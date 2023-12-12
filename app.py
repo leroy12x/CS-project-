234cb1198bcfbb770b1fc8e864ce9bc76ea2d230
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-12-12 16:21:14 +0100

    ok

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def display_task_manager():
         if task_ects and task_ects.strip():
             task_ects = int(task_ects)
         if task_id is not None:
-            term_id = task_id # das war es glaube ich 
+            term_id = task_id
             events_df = get_events_by_term(term_id)
             # Filter events by the provided course ID
             if not events_df.empty:
@@ -138,7 +138,7 @@ def display_task_manager():
                     course_events = events_df[events_df['id'] == course_id]
                     title_list = course_events['title'].tolist()
                     if title_list and isinstance(title_list[0], str):
-                        task_name = title_list[0]  # Set the title as task name
+                        task_name = title_list[0]  # Set the title as task description
                     max_credits_list = course_events['maxCredits'].tolist()
                     if max_credits_list and isinstance(max_credits_list[0], list) and len(max_credits_list[0]) > 0:
                         task_ects = (int(max_credits_list[0][0])/100) # Set maxCredits as ECTS
@@ -379,3 +379,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
