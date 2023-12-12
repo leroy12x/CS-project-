7e0f260c63526f947d5e1077c9bb83d54679cd4c
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-12-12 19:31:08 +0100

    <sc

--- app.py
+++ app.py
@@ -74,22 +74,24 @@ def display_to_do():
         st.subheader(f"Tasks for {day}")
 
            # Handle cases where remaining_hours might be None
-        remaining_hours = task.get('remaining_hours')
+        remaining_hours = tasks.get('remaining_hours')
         if remaining_hours is None:
+            task_name = tasks['name']
+            task_time = tasks['time']
                 # Recalculate remaining hours if not available
-                task_ects = float(task['ects'])
-                task_percentage = float(task['percentage'])
-                total_ects = round(task_ects * (task_percentage / 100), 2)
-                remaining_hours = total_ects * 30  # Multiply ECTS by 30 to estimate work hours
+            task_ects = float(tasks['ects'])
+            task_percentage = float(tasks['percentage'])
+            total_ects = round(task_ects * (task_percentage / 100), 2)
+            remaining_hours = total_ects * 30  # Multiply ECTS by 30 to estimate work hours
  
             # Check if task is completed
-        if task.get('completed', False):
+        if tasks.get('completed', False):
                 st.markdown(f"<span style='color: green;'>{task_name} - Completed on: {task_time}</span>", unsafe_allow_html=True)
         else:
-                overdue = datetime.strptime(task['due_date'], '%Y-%m-%d') < datetime.now()
+                overdue = datetime.strptime(tasks['due_date'], '%Y-%m-%d') < datetime.now()
                 color = "red" if overdue else "orange"
                 st.markdown(f"<span style='color: {color};'>{task_name} ({task['total_ects']} ECTS) - Due: {task_time}{' (Overdue)' if overdue else ''}</span>", unsafe_allow_html=True)
-                st.write(f"Estimated Remaining Work Hours: {ects_task} hours")
+                st.write(f"Estimated Remaining Work Hours: {remaining_hours} hours")
     
                     
                     
