27f973178f1bc2ebbc84e6124f8b82fb5e1f501e
markklb <152989960+markklb@users.noreply.github.com>
2023-12-12 16:09:01 +0100

    n

--- app.py
+++ app.py
@@ -140,8 +140,7 @@ def display_task_manager():
                       # Set the title as task description
                     max_credits_list = course_events['maxCredits'].tolist()
                     if max_credits_list and isinstance(max_credits_list[0], list) and len(max_credits_list[0]) > 0:
-                        task_ects = (int(max_credits_list[0][0])/100)  # Set maxCredits as ECTS
-
+                    task_ects = (int(max_credits_list[0][0])/100)  # Set maxCredits as ECTS
                     else:
                         st.error(f"No maxCredits found for Course ID: {course_id}")
                 else:
