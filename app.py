2be0b7887ab692b8d95c2a0d368eccddd9510e14
leroy12x <150723142+leroy12x@users.noreply.github.com>
2023-12-14 13:10:20 +0100

    ydfhg

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_current_semester():
         "X-ApplicationId": "587acf1c-24d0-4801-afda-c98f081c4678",
         "API-Version": "1",
         "X-RequestedLanguage": "de"}
-    
+
     response = requests.get(url, headers=headers)
     if response.ok:
         return response.json()
@@ -29,10 +29,9 @@ semester_info = get_current_semester()
 if semester_info:
     # Extract and display the description from the semester information
     semester_description = semester_info.get('description', 'No description available')
-    st.write(f"{semester_description}")
 else:
     st.error("Failed to fetch current semester information.")
-
+    st.write("semester_description")
 semester_ids = get_current_semester()
 if semester_ids:
     # Extract and display the description from the semester information
@@ -66,7 +65,7 @@ def display_to_do():
     tasks = load_tasks_from_csv()
     st.session_state.tasks = tasks   # Initialize session state
 
-    st.title("Tasks with ECTS and Time Estimates")
+    st.markdown("<h2>Tasks with ECTS and Time Estimates</h2>", unsafe_allow_html=True)
     tasks = load_tasks_from_csv()
     calculate_ects_percentage(tasks)
 
@@ -102,38 +101,38 @@ def display_to_do():
                     save_tasks_to_csv(tasks)  # Speichere die aktualisierten Aufgaben
                     st.experimental_rerun()  # Seite neu laden, um Änderungen anzuzeigen
 
-                #
-
-
-    
-                    
-                    
-        
                 
+
+
+
+
+
+
+
 def display_task_manager():
-    st.title("Create Tasks")  # Renamed from "Task Manager"
+    st.markdown("<h2>Create Tasks</h2>", unsafe_allow_html=True)
     # Input field for course ID
     # Set default allocated time to 1 hour
     placeholder_text_time = "Enter the Time in the format 12:00"
     task_allocated_time = st.text_input("Deadline", placeholder=placeholder_text_time,key="task_allocated_time")
-    task_due_date = st.date_input("Select Due Date", key="task_due_date") 
+    task_due_date = st.date_input("Select Due Date", key="task_due_date")
     placeholder_text_name = "Copy your COURSE NAME"
     task_name = st.text_input("Enter Task Name",placeholder=placeholder_text_name, key="task_name")
     task_description = st.text_input("Enter Task Description", key="task_description")
     placeholder_text_ects = "Leave blank when you have a COURSE NAME"
-    task_ects = st.text_input("Enter ECTS Points",placeholder=placeholder_text_ects, key="task_ects")      
+    task_ects = st.text_input("Enter ECTS Points",placeholder=placeholder_text_ects, key="task_ects")
     task_percentage = st.text_input("Enter Percentage of Grade", key="task_percentage")
-            
+
     if st.button("Add Task"):
-        
-           
+
+
         tasks = load_tasks_from_csv()
         start_time = datetime.strptime(task_allocated_time, "%H:%M").time()  # Just time component
         start_date_time = get_datetime_on_date(task_due_date, start_time)
         task_percentage = int(task_percentage)
         if task_ects and task_ects.strip():
             task_ects = float(task_ects)
-        if task_name is not None: 
+        if task_name is not None:
             events_df = get_events_by_term(semester_id)
             # Filter events by the provided course ID
             if not events_df.empty:
@@ -154,15 +153,15 @@ def display_task_manager():
             else:
                 st.error("No events data available.")
         else:
-            st.warning('Please enter a Course ID.') 
-            
+            st.warning('Please enter a Course ID.')
+
         task_info = {
             'time': start_date_time.strftime("%H:%M"),
             'description': task_description,
             'name': task_name,
             'ects': task_ects,
             'percentage': task_percentage,
-            'due_date': task_due_date.strftime('%Y-%m-%d'), 
+            'due_date': task_due_date.strftime('%Y-%m-%d'),
             'remaining_hours':(task_ects*(task_percentage/100))*30, # Format the date
             'completed': False  # Correctly placed inside task_info
         }
@@ -172,7 +171,7 @@ def display_task_manager():
             tasks[date_key].append(task_info)
         else:
             tasks[date_key] = [task_info]
-        
+
         # Save tasks to the CSV file
         save_tasks_to_csv(tasks)
 
@@ -180,9 +179,9 @@ def display_task_manager():
 
         st.experimental_rerun()
 
-       
+
 def get_datetime_on_date(date, time):
-    # Kombiniere das Datum mit der Uhrzeit, um ein kombiniertes datetime-Objekt zu erhalten
+    # Combine the date with the time to obtain a combined datetime object
     combined_datetime = datetime.combine(date, time)
     return combined_datetime
 
@@ -192,9 +191,9 @@ def get_datetime_on_date(date, time):
 def save_tasks_to_csv(tasks):
     df = pd.DataFrame([
         (
-            key[0], key[1], key[2], 
-            task['time'], task['name'], task['description'], 
-            task['ects'], task['percentage'], task['due_date'], 
+            key[0], key[1], key[2],
+            task['time'], task['name'], task['description'],
+            task['ects'], task['percentage'], task['due_date'],
             task.get('completed', False), task.get('remaining_hours', None)  # Added remaining_hours
         )
         for key, tasks_list in tasks.items() for task in tasks_list
@@ -246,7 +245,7 @@ def calculate_ects_percentage(tasks):
 
 # Function to edit or delete tasks
 def edit_tasks():
-    st.title("Edit Tasks")
+    st.markdown("<h2>Edit Tasks</h2>", unsafe_allow_html=True)
 
     tasks = load_tasks_from_csv()
     task_list = [f"{task['name']} (Due: {task['due_date']})" for day_tasks in tasks.values() for task in day_tasks]
@@ -275,7 +274,7 @@ def edit_tasks():
             selected_task_details['percentage'] = new_percentage
             save_tasks_to_csv(tasks)
             st.success("Task updated successfully!")
-            
+
 
         if st.button("Delete Task"):
             # Remove the selected task from the list of tasks for that day
@@ -285,24 +284,24 @@ def edit_tasks():
                 del tasks[selected_date_key]
             save_tasks_to_csv(tasks)
             st.success("Task deleted successfully!")
-        
-        
-        
-        
-            
 
 
-                    
+
+
+
+
+
+
 
 
 
     # Function to handle marking tasks as completed
 def mark_as_completed(task_name, due_date_str):
-    # Konvertiere das Datum in ein datetime Objekt
+    # Convert the date into a datetime object
     due_date = datetime.strptime(due_date_str, '%Y-%m-%d')
     date_key = (due_date.year, due_date.month, due_date.day)
 
-    # Überprüfe, ob das Datum als Schlüssel existiert
+    # Check whether the date exists as a key
     if date_key in st.session_state.tasks:
         for task in st.session_state.tasks[date_key]:
             if task['name'] == task_name and task['due_date'] == due_date_str:
@@ -313,7 +312,7 @@ def mark_as_completed(task_name, due_date_str):
 
 
 def display_weekly_calendar():
-    st.title("Weekly Calendar")
+    st.markdown("<h2>Weekly Calendar</h2>", unsafe_allow_html=True)
     tasks = load_tasks_from_csv()  # Ensure this function returns a dictionary of tasks
 
     today = datetime.today()
@@ -346,7 +345,7 @@ def display_weekly_calendar():
                         task['total_ects'] = round(task_ects * (task_percentage / 100), 2)
                         # Task description with time, name, and other details
                         task_display = f"{task['time']} - {task['name']}: {task['description']} (ECTS: {task['ects']}, Percentage: {task['percentage']})"
-                        
+
                         # Apply color styling based on the task status
                         if completed:
                             st.markdown(f"<span style='color: green;'>{task_display}</span>", unsafe_allow_html=True)
@@ -357,11 +356,11 @@ def display_weekly_calendar():
                 else:
                     st.write("No tasks")
 
-# Anpassung der main-Funktion, um die neue Funktion aufzurufen
+
 
 # Function to record hours worked and subtract them from the Estimated Remaining Work Hours
 def display_work_done():
-    st.title("Record Work")
+    st.markdown("<h2>Record Work</h2>", unsafe_allow_html=True)
 
     tasks = load_tasks_from_csv()  # Load existing tasks
 
@@ -380,8 +379,8 @@ def display_work_done():
 
     if selected_task_details:
         hours_worked = st.number_input("Enter hours worked", min_value=0.0, step=0.5)
-        
-        if st.button("Record Work"):
+
+        if st.button("Record Hours"):
             # Calculate and update the remaining hours
             task_ects = float(selected_task_details['ects'])
             task_percentage = float(selected_task_details['percentage'])
@@ -391,7 +390,7 @@ def display_work_done():
 
             # Update task info
             selected_task_details['remaining_hours'] = remaining_hours
-            
+
             # Save the updated tasks
             save_tasks_to_csv(tasks)
             st.success(f"Updated remaining work hours for '{selected_task_details['name']}' to {remaining_hours} hours.")
@@ -402,22 +401,45 @@ def initialize_session_state():
     if 'tasks' not in st.session_state:
         st.session_state.tasks = load_tasks_from_csv()
 
-def main():
-    initialize_session_state()
-    st.sidebar.title("Navigation")
-    app_mode = st.sidebar.selectbox("Choose a Page", ["Create Tasks", "To Do List", "Edit Tasks", "Weekly Calendar","Record Work"])
+def display_front_page():
+    st.markdown("<h1 style='text-align: center; color: green;'>Welcome to StudySprint</h1>", unsafe_allow_html=True)
+    st.write("<h4 style='text-align: center;'>Never forget your tasks again!</h4>", unsafe_allow_html=True)
 
-    if app_mode == "Create Tasks":
+    st.markdown("---")  # Divider
+
+    st.markdown("<h3 style='text-align: center;'>Features:</h3>", unsafe_allow_html=True)
+    st.markdown(
+        "<div style='text-align: center;'>"
+        "<ul style='list-style-position: inside; padding-left: 0; display: inline-block; text-align: left;'>"
+        "<li>Task Management</li>"
+        "<li>To Do List</li>"
+        "<li>Weekly Calendar</li>"
+        "</ul>"
+        "</div>", unsafe_allow_html=True)
+
+    st.markdown("---")  # Divider
+
+    st.markdown("<p style='text-align: center;'>Start your journey with StudySprint by selecting an option from the sidebar.</p>", unsafe_allow_html=True)
+
+def display_navigation():
+    st.sidebar.markdown("<h2 style='color: green;'>Navigation</h2>", unsafe_allow_html=True)
+    selected_option = st.sidebar.radio("", ["Front Page", "Create Tasks", "Edit Tasks", "Record Work", "To Do List", "Weekly Calendar"])
+
+    if selected_option == "Front Page":
+        display_front_page()
+    elif selected_option == "Create Tasks":
         display_task_manager()
-    elif app_mode == "To Do List":
-        display_to_do()
-    elif app_mode == "Edit Tasks":
+    elif selected_option == "Edit Tasks":
         edit_tasks()
-    elif app_mode == "Weekly Calendar":
-        display_weekly_calendar()
-    elif app_mode == "Record Work":
+    elif selected_option == "Record Work":
         display_work_done()
+    elif selected_option == "To Do List":
+        display_to_do()
+    elif selected_option == "Weekly Calendar":
+        display_weekly_calendar()
 
+def main():
+    display_navigation()
 
 if __name__ == "__main__":
     main()
